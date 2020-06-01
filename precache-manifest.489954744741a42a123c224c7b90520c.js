fb6b3795c64fffa0610a4fb8e348d225d5b472fd
Mithun KT <mithun.thachareyath@akqa.com>
2020-06-01 23:02:19 +1000

    Updates

---
+++ precache-manifest.489954744741a42a123c224c7b90520c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385a4805edf23f8d39d0a81455f1f093",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "613287e623570011dc1a",
+    "url": "/bingo/static/css/main.8b164fe5.chunk.css"
+  },
+  {
+    "revision": "ccb361b5f8931856c5a7",
+    "url": "/bingo/static/js/2.f57e83c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo/static/js/2.f57e83c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613287e623570011dc1a",
+    "url": "/bingo/static/js/main.23fa53dc.chunk.js"
+  },
+  {
+    "revision": "e60f173697a37915a237",
+    "url": "/bingo/static/js/runtime-main.0401bbb8.js"
+  }
+]);
